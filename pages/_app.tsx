01aeaebaf24060f65c4fb7e988aaa58762173de6
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-05 16:44:45 -0300

    added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Partap</title>
     </Head>
     <ThemeProvider>
       <Component {...pageProps} />
