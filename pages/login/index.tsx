241144224cdceb5fc8ea80611bccdf86fe2cc6a1
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-05 18:14:49 -0300

    Login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, Flex, FormControl, FormLabel, Grid, Heading, Input, SimpleGrid, Text, useToast } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, FormControl, FormLabel, Grid, Heading, Icon, Input, SimpleGrid, Text, useToast } from '@chakra-ui/react'
 import { useRouter } from 'next/router';
 import React, { useContext, useEffect, useState } from 'react'
 import { useForm } from "react-hook-form";
 import { ApiInstance } from '../../services/api';
 import { OwnAPI } from '../../services/own-api';
 import useUser from '../../lib/useUser';
+import { FaHome } from 'react-icons/fa';
 
 export default function Home() {
   const router = useRouter()
@@ -62,50 +63,67 @@ export default function Home() {
   }, [signUpForm])
 
   return (
-    <SimpleGrid
+    <Center
       w={"100vw"}
       h={"100vh"}
-      columns={{ base: 1, md: 3 }}
-      p={2}
       alignItems="center"
       justifyItems={"center"}
     >
-      <div></div>
       <Flex
         alignSelf={"center"}
         direction="column"
-        gap={2}
+        boxShadow="lg"
+        borderRadius="md"
       >
-        <Heading size={"2xl"} color="purple.500" textAlign="center">Partap</Heading>
-        <Heading fontSize={'medium'} textAlign="center">Faça login para entrar</Heading>
-        {!signUpForm && <>
-          <form onSubmit={handleSubmit(handleSignIn)}>
-            <Flex w="xs" direction="column" gap={2}>
-              <Input required placeholder='E-mail' {...register('email')} />
-              <Input required placeholder='Password' type="password" {...register('password')} />
-              {errorMsg && <Text fontSize="sm" color="red.500" textAlign="center">{errorMsg}</Text>}
-              <Button
-               colorScheme='green'
-               isLoading={isLoading}
-               type="submit">Entrar</Button>
-            </Flex>
-          </form>
-          <Button size="sm" type="submit" onClick={() => setSignUpForm(true)}>Cadastrar</Button>
-        </>}
-        {signUpForm && <>
-          <form onSubmit={handleSubmit(handleSignUp)}>
-            <Flex w="xs" direction="column" gap={2}>
-              <Input required placeholder='Nome' {...register('name')} />
-              <Input required placeholder='E-mail' {...register('email')} />
-              <Input required placeholder='Password' type="password" {...register('password')} />
-              {errorMsg && <Text fontSize="sm" color="red.500" textAlign="center">{errorMsg}</Text>}
-              <Button colorScheme='green' isLoading={isLoading} type="submit">Cadastrar</Button>
-            </Flex>
-          </form>
-          <Button size="sm" type="submit" onClick={() => setSignUpForm(false)}>Voltar ao login</Button>
-        </>}
+        <Flex
+          bgGradient='linear-gradient(to-r, pink.400, pink.600)'
+          direction="column"
+          alignItems={"center"}
+          py={4}
+          borderTopRadius="lg"
+        >
+          <Center flexDirection="column">
+            <Icon as={FaHome} h={12} w={12} color="white" />
+            <Heading size={"2xl"} color="white" textAlign="center">Partap</Heading>
+            <Text w="70%" wordBreak={'break-word'} color="white" textAlign="center" fontSize="sm" mt={2}>Seu novo jeito de organizar a procura por imóveis</Text>
+          </Center>
+        </Flex>
+        <Flex
+          direction="column"
+          gap={2}
+          py={8}
+          px={12}
+        >
+          <Heading fontSize={'medium'} textAlign="center">Faça login para entrar</Heading>
+          {!signUpForm && <>
+            <form onSubmit={handleSubmit(handleSignIn)}>
+              <Flex w="xs" direction="column" gap={2}>
+                <Input required placeholder='E-mail' {...register('email')} />
+                <Input required placeholder='Senha' type="password" {...register('password')} />
+                {errorMsg && <Text fontSize="sm" color="red.500" textAlign="center">{errorMsg}</Text>}
+                <Button
+                  colorScheme='green'
+                  isLoading={isLoading}
+                  type="submit">Entrar</Button>
+              </Flex>
+            </form>
+            <Button size="sm" variant="ghost" type="submit" onClick={() => setSignUpForm(true)}>Cadastrar</Button>
+          </>}
+          {signUpForm && <>
+            <form onSubmit={handleSubmit(handleSignUp)}>
+              <Flex w="xs" direction="column" gap={2}>
+                <Input required placeholder='Nome' {...register('name')} />
+                <Input required placeholder='E-mail' {...register('email')} />
+                <Input required placeholder='Senha' type="password" {...register('password')} />
+                {errorMsg && <Text fontSize="sm" color="red.500" textAlign="center">{errorMsg}</Text>}
+                <Button colorScheme='green' isLoading={isLoading} type="submit">Cadastrar</Button>
+              </Flex>
+            </form>
+            <Button size="sm" type="submit" onClick={() => setSignUpForm(false)}>Voltar ao login</Button>
+          </>}
+        </Flex>
+
       </Flex>
-      <div></div>
-    </SimpleGrid>
+    </Center>
   )
 }
