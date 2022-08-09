4af121df26d8336d334f9dbcbf06811fdadb17ed
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-09 14:54:52 -0300

    Changed the API

--- config.ts
+++ config.ts
@@ -1,3 +1,2 @@
-// export const ApiURL = "https://q7wi8vce1g.execute-api.us-east-1.amazonaws.com";
-export const ApiURL = "https://bh6quscug0.execute-api.us-east-1.amazonaws.com";
+export const ApiURL = "https://q7wi8vce1g.execute-api.us-east-1.amazonaws.com";
 // export const ApiURL = 'http://localhost:4000'
